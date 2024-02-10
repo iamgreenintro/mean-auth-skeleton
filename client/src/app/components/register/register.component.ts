70c5310debc0932a1536f6a02bb3bb9bb8b4ac06
iamgreenintro <iamgreenintro@outlook.com>
2024-02-10 21:06:40 +0100

    restore register method

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RegisterModules } from './register.module';
 import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 // import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'register',
   standalone: true,
   imports: [RegisterModules],
-  providers: [],
+  providers: [UserService],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
@@ -16,13 +17,15 @@ export class RegisterComponent {
   public password: string = '';
   public isPasswordHidden: boolean = true;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private userService: UserService) {}
 
   public register = async (): Promise<void> => {
     const credentials = { username: this.username, password: this.password };
     console.log(credentials);
 
-    // const response = await this.userService.createUser(credentials);
-    // console.log(response);
+    const response = await this.userService.createUser(credentials);
+    if (!response.error) {
+      this.router.navigate(['/login']);
+    }
   };
 }
